879d755d3befe6f769197da3e3d255adfade8f1e
Zygimantas Macijauskas <zigglymantis@gmail.com>
2019-12-05 21:11:03 -0500

    added same text to indexMenu

--- public/js/indexMenu.js
+++ public/js/indexMenu.js
@@ -34,16 +34,14 @@ var API = {
 var refreshOrders = function() {
   API.getOrders().then(function(data) {
     var $orders = data.map(function(order) {
-      var $a = $("<a>")
-        .text(order.text)
-        .attr("href", "/orders/" + order.id);
-
+      var $p = `<p>Item: ${order.Item}</p><p>Price: ${order.Cost}</p><p>Quantity: ${order.Quantity}</p><p>Status: ${order.Status}</p>`
+      
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
           "data-id": order.id
         })
-        .append($a);
+        .append($p);
 
       var $button = $("<button>")
         .addClass("btn btn-danger float-right delete")
