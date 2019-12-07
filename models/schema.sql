c4f71547dd79ab9cfceff2f96df07c2fa7ed6734
Zygimantas Macijauskas <zigglymantis@gmail.com>
2019-12-07 11:53:01 -0500

    added testing info to schema

--- models/schema.sql
+++ models/schema.sql
@@ -2,8 +2,21 @@ DROP DATABASE IF EXISTS gitOrder;
 CREATE DATABASE gitOrder;
 use gitorder;
 
-INSERT INTO menus (Item, Ingredients, Cost) VALUES ("Homewrecker", "guac, seasoned rice, beans, shredded cheese, steak", 10);
-
+insert into menus (Name, Ingredients, Cost) values ("Homewrecker", "guac, seasoned rice, beans, shredded cheese, steak, tortilla", 10);
+insert into menus (Name, Ingredients, Cost) values ("Wrong-Doug", "queso, pico de gallo, beans, shredded cheese, steak, tortilla, crunchy corn shell x2", 9);
+insert into menus (Name, Ingredients, Cost) values ("Big Mac", "pickles, onions, cheese, lettuce, big mac sauce, hamburger x2, sesame seed buns", 5);
+insert into menus (Name, Ingredients, Cost) values ("Bacon Cheeseburger", "beef burger, bacon, cheese, bun", 11);
+insert into menus (Name, Ingredients, Cost) values ("Chicken Popeyes", "chicken", 7);
+insert into menus (Name, Ingredients, Cost) values ("Chicken Sandwich", "breaded chicken, soft bun, tart pickles", 4);
+insert into menus (Name, Ingredients, Cost) values ("Spicy Chicken Sandwich", "breaded spicy chicken, soft bun, tart pickles, pepperjack cheese", 4);
+insert into menus (Name, Ingredients, Cost) values ("Mcnuggets", "chicken", 6);
+insert into menus (Name, Ingredients, Cost) values ("Chicken Fingers", "seasoned breaded chicken", 2);
+insert into menus (Name, Ingredients, Cost) values ("Cheesy Gordita Crunch", "double tortilla, hard soft shell, cheese, ground beef, lettuce, baha sauce", 4);
+insert into menus (Name, Ingredients, Cost) values ("Soft Tacos", "soft tortilla, ground beef, cheese, sour cream, guac", 8);
+insert into menus (Name, Ingredients, Cost) values ("Roast Beef Sandwich", "sesame bun, cold cut roast beef, cheddar cheese, arby's sauce", 4);
+insert into menus (Name, Ingredients, Cost) values ("Chicken Quesadilla", "tortilla, cheese, chicken, roasted onions, roasted peppers, tangy spicy sauce", 4);
+insert into menus (Name, Ingredients, Cost) values ("Filet-O-Fish", "square fish, american cheese, soft bun, tarter sauce", 5);
+insert into menus (Name, Ingredients, Cost) values ("Frosty", "milk, chocolate, sugar, icecream", 3);
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
